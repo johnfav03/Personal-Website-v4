0c42862b07dbd98113d4c3128714bcc9b50c7f24
John Favret <john.favret@energysage.com>
2023-06-21 13:29:07 -0400

    removed img filepaths

--- src/App.vue
+++ src/App.vue
@@ -22,55 +22,55 @@
                 <h1>PROJECTS</h1>
                 <div class="proj-grid">
                     <ProjCard 
-                        image="src/stick-sim.png"
+                        image="stick-sim.png"
                         title="STICK SHIFT SIM"
                         messg="This was the final project for EECS 498, a self-led project, which utilized VR through Unreal to deliver an immersive manual driving trainer, complete with realistic physics and grip controls."
                         date="WN 2023"
                     />
                     <ProjCard 
-                        image="src/a2-go.png"
+                        image="a2-go.png"
                         title="ANN ARBOR GO!"
                         messg="This was the second project for EECS 498, which uses Unity and Mapbox to replicate both the geolocational and AR features of Pokemon Go, but themed for Ann Arbor."
                         date="WN 2023"
                     />
                     <ProjCard 
-                        image="src/vue-weather.png"
+                        image="vue-weather.png"
                         title="WEATHER APP"
                         messg="Developed in Vue.js, this project was started to learn front-end frameworks and API calls. The web app features geolocational search as well as live weather and temperature from the OpenWeather API."
                         date="FA 2022"
                     />
                     <ProjCard 
-                        image="src/django-dz.png"
+                        image="django-dz.png"
                         title="IMAGE DROPZONE"
                         messg="This project was meant to help learn backend concepts, and uses a simple front-end with Dropzone.js to pass images to Django, so they can be cached and displayed on the next page."
                         date="WN 2022"
                     />
                     <ProjCard 
-                        image="src/wordle.png"
+                        image="wordle.png"
                         title="WORDLE EVALUATOR"
                         messg="Utilizing the terminal, this program analyzed starting words, either from input or a database, for both positional and non-specific likelihood, giving each a score and storing the top results. "
                         date="FA 2021"
                     />
                     <ProjCard 
-                        image="src/eng-club.png"
+                        image="eng-club.png"
                         title="ENGINEERING CLUB"
                         messg="I founded and nurtured this organization in high school with a close friend, where I took up the position of Education Chair teaching C++ and basic circuity targeted at Arduino Microcontrollers."
                         date="WN 2020"
                     />
                     <ProjCard 
-                        image="src/wii-nunchuk.png"
+                        image="wii-nunchuk.png"
                         title="NUNCHUK BATTLESHIP"
                         messg="In this project, I rewired an old Ninento Nunchuk to and Arduino along with an 8x8 LED matrix, allowing the user to play Battleship against a randomly guessing computer player."
                         date="FA 2018"
                     />
                     <ProjCard 
-                        image="src/flappy-bird.png"
+                        image="flappy-bird.png"
                         title="FLAPPY BIRD"
                         messg="This was the capstone project for my first computer science course, and used Java and Greenfoot to simulate the then-popular mobile game, flappy bird, using custom sprites and adjustable gravity."
                         date="FA 2017"
                     />
                     <ProjCard 
-                        image="src/valentine.png"
+                        image="valentine.png"
                         title="V-DAY OPERATION"
                         messg="This was a device I made in middle school as a Valentine's Day gift; it was a themed version of the game 'operation', triggering a buzzer if the pair of tweezers contacted the metal heart."
                         date="WN 2016"
